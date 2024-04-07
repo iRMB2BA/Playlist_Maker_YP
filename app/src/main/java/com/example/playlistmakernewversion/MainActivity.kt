29a144322b83286405334471cd499cc62e2453de
Alexandr R <irombaba@gmail.com>
2024-04-07 16:26:22 +0300

    Домашка 9

--- app/src/main/java/com/example/playlistmakernewversion/MainActivity.kt
+++ app/src/main/java/com/example/playlistmakernewversion/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.example.playlistmakernewversion
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        buttonSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+
+        buttonMedia.setOnClickListener {
+            val intent = Intent(this, MediaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+
+        buttonSearch.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
