572ca9a24bff6ea895abea1c5b4a30690392e1be
Alexandr R <irombaba@gmail.com>
2024-04-11 08:08:06 +0300

    Домашка 10

---
+++ app/src/main/java/com/example/playlistmakernewversion/TrackAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.playlistmakernewversion
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackAdapter(private val songs: List<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.track_view_card, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(songs[position])
+    }
+
+    override fun getItemCount() = songs.size
+}
