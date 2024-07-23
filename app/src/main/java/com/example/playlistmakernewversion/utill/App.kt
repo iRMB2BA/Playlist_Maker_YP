127206a8c264b68b1f07fec6107843710a373f08
Alexandr R <irombaba@gmail.com>
2024-07-23 08:50:55 +0300

    Спринт 16.04 Setting fix

--- app/src/main/java/com/example/playlistmakernewversion/utill/App.kt
+++ app/src/main/java/com/example/playlistmakernewversion/utill/App.kt
@@ -1,15 +1,12 @@
 package com.example.playlistmakernewversion.utill
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import com.example.playlistmakernewversion.Creator.Creator
 import com.example.playlistmakernewversion.settings.domain.api.SettingInteractor
-import com.example.playlistmakernewversion.settings.ui.activity.KEY_PREF
-import com.example.playlistmakernewversion.settings.ui.activity.PLAYLIST_PREF
 
 class App : Application() {
 
-    private lateinit var interactor : SettingInteractor
+    private lateinit var interactor: SettingInteractor
 
     override fun onCreate() {
         super.onCreate()
